725e46987342dd6c555dd712535885409710f639
Tim01Bro <arthaos14@gmail.com>
2023-11-23 12:12:06 +0300

    Config key stripe

--- src/main/java/io/fooddelivery/configuration/AppConfig.java
+++ src/main/java/io/fooddelivery/configuration/AppConfig.java
@@ -1,14 +1,23 @@
 package io.fooddelivery.configuration;
 
+import com.stripe.Stripe;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.annotation.PostConstruct;
+
 @Configuration
 @ComponentScan("io.fooddelivery")
 public class AppConfig {
-
+    @Value("${stripe.api.secretKey}")
+    private String secretKey;
+    @PostConstruct
+    public void  initSecretKey(){
+        Stripe.apiKey = secretKey;
+    }
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder(8);
